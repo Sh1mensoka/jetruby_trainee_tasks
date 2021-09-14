9b83382b22d00edb37a1fbebef09ec901d59dfc1
Mikhail Bashchenko <mikhail.bashchenko@jetruby.com>
2021-09-14 12:55:29 +0300

    [TRELLO-10] Added organisation entity and edited user entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_080217) do
+ActiveRecord::Schema.define(version: 2021_09_14_094624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,12 @@ ActiveRecord::Schema.define(version: 2021_09_02_080217) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "organisations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -78,9 +84,13 @@ ActiveRecord::Schema.define(version: 2021_09_02_080217) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar"
+    t.string "role"
+    t.bigint "organisation_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "orders", "users"
+  add_foreign_key "users", "organisations"
 end
